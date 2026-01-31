1d4ee22700ca6803d68fca2f87dcfdaa72da306e
Johannes <freelon@gmx.net>
2026-01-31 20:33:27 +0100

    more tests

--- test/integration_test.rs
+++ test/integration_test.rs
@@ -4,6 +4,7 @@ use std::{
     process::{Command, Output},
 };
 
+use chrono::Local;
 use tempdir::TempDir;
 
 #[test]
@@ -24,13 +25,15 @@ fn shows_help() {
 
 #[test]
 fn new_name() {
-    let h = Harness::new();
-    let p = h.run_try(&["foo"]);
+    // given
+    let h = Harness::new(false);
 
+    // when
+    let p = h.run_try(&["foo"]);
     let output = String::from_utf8(p.stdout).unwrap();
 
+    // then
     let expected_dir = h.path().join("foo");
-    h.dir.into_path();
     assert_eq!(
         format!("cd '{}'", expected_dir.display()),
         output.trim(),
@@ -40,18 +43,86 @@ fn new_name() {
     assert!(expected_dir.is_dir(), "try dir is a directory");
 }
 
+#[test]
+fn exact_folder_exists() {
+    // given
+    let h = Harness::new(false);
+    let existing = "existing";
+    h.create_try_folder(existing);
+
+    // when
+    let p = h.run_try(&[existing]);
+    let output = String::from_utf8(p.stdout).unwrap();
+
+    // then
+    let expected_dir = h.path().join(existing);
+    assert_eq!(
+        format!("cd '{}'", expected_dir.display()),
+        output.trim(),
+        "printed command is correct"
+    );
+}
+
+#[test]
+fn folder_with_date_exists() {
+    // given
+    let h = Harness::new(false);
+    let name = "2020-02-02 existing";
+    h.create_try_folder(name);
+
+    // when
+    let p = h.run_try(&["existing"]);
+    let output = String::from_utf8(p.stdout).unwrap();
+
+    // then
+    let expected_dir = h.path().join(name);
+    assert_eq!(
+        format!("cd '{}'", expected_dir.display()),
+        output.trim(),
+        "printed command is correct"
+    );
+}
+
+#[test]
+fn new_name_with_date() {
+    // given
+    let h = Harness::new(true);
+    let name = "existing";
+
+    // when
+    let p = h.run_try(&["existing"]);
+    let output = String::from_utf8(p.stdout).unwrap();
+
+    // then
+    let expected_dir = h.path().join(format!(
+        "{} {}",
+        Local::now().format("%Y-%m-%d").to_string(),
+        name
+    ));
+    assert_eq!(
+        format!("cd '{}'", expected_dir.display()),
+        output.trim(),
+        "printed command is correct"
+    );
+}
+
 struct Harness {
     dir: TempDir,
 }
 
 impl Harness {
-    fn new() -> Self {
+    fn new(with_date_prefix: bool) -> Self {
         let dir = TempDir::new("try-test").expect("couldn't generate temp directory for test");
-        fs::write(
-            dir.path().join("config.toml"),
-            format!("tries_path = \"{}\"", dir.path().join("tries").display()),
-        )
-        .expect("could not create config file");
+        let mut config = String::new();
+        config.push_str(&format!(
+            "tries_path = \"{}\"\n",
+            dir.path().join("tries").display()
+        ));
+        if with_date_prefix {
+            config.push_str("apply_date_prefix = true\n");
+        }
+        dbg!(&config);
+        fs::write(dir.path().join("config.toml"), config).expect("could not create config file");
         Harness { dir }
     }
 
@@ -67,5 +138,12 @@ impl Harness {
 
     fn path(&self) -> PathBuf {
         self.dir.path().join("tries").to_path_buf()
+    }
+
+    fn create_try_folder(&self, name: &str) {
+        fs::DirBuilder::new()
+            .recursive(true)
+            .create(self.path().join(name))
+            .expect("couldn't create 'existing' try");
     }
 }
